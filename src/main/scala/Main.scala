5f68645e7f3a31f512812968736cf06ba7dc1e0c
h <ma>
2024-05-17 14:42:49 +0200

    Stream 1 & Stream 2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,7 +7,8 @@ object Main extends App {
   println("Rapports des données IoT :")
   println("=" * 75)  // Affiche une ligne de séparation
 
-  // Ajout du shutdown hook ici pour s'assurer qu'il est enregistré une seule fois
+  KafkaStreamProcessing.startStream() // Démarrer le traitement du stream Kafka
+
   sys.addShutdownHook {
     MyKafkaProducer.close()
     println("Fermeture du producteur Kafka.")
@@ -23,14 +24,12 @@ object Main extends App {
       val alerte = "No"
       val rapport = SimulateurIoT.simulerRapportIoT(deviceId, currentTime, loc, alerte)
 
-      // Affichage et traitement du rapport
       print("------------Serialisation JSON------------")
       val json = IoTDataJson.serialize(rapport)
       println(s"\nJSON:\n$json")
 
-      // Envoyer le rapport sérialisé à Kafka
       try {
-        MyKafkaProducer.sendIoTData("iot-data-topic", rapport.deviceId, json) // iot-data-topic = nom du topic 
+        MyKafkaProducer.sendIoTData("iot-data-topic", rapport.deviceId, json) 
       } catch {
         case e: Exception => println(s"Erreur lors de l'envoi à Kafka: ${e.getMessage}")
       }
@@ -38,8 +37,7 @@ object Main extends App {
       val deserializedData = IoTDataJson.deserialize(json)
       deserializedData match {
         case Right(data) =>
-          println("\n")
-          println("------------Désérialisation JSON------------")
+          println("\n------------Désérialisation JSON------------")
           println(s"Rapport IoT pour le dispositif ${data.deviceId} à l'instant ${data.timestamp}")
           println(s"Localisation : ${data.location.capital}, Latitude ${data.location.latitude}°, Longitude ${data.location.longitude}°")
           println(s"Qualité de l'Air : CO2 ${data.qualiteAir.CO2} ppm, Particules fines ${data.qualiteAir.particulesFines} µg/m³")
@@ -69,6 +67,6 @@ object Main extends App {
 
       println("-" * 100) // Ligne de séparation entre les rapports
     }
-    Thread.sleep(600000) // Pause pour 10 minutes (600000 millisecondes)
+    Thread.sleep(150000) // Pause pour 2'30 minutes
   }
 }
--- src/main/scala/kafka/KafkaStreamProcessing.scala
+++ src/main/scala/kafka/KafkaStreamProcessing.scala
@@ -2,12 +2,11 @@ import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.{KStream, Consumed, Produced}
 import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 import java.util.Properties
 
 object KafkaStreamProcessing {
-  def main(args: Array[String]): Unit = {
+  def startStream(): KafkaStreams = {
     val props = new Properties()
     props.put(StreamsConfig.APPLICATION_ID_CONFIG, "iot-data-processor")
     props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
@@ -15,31 +14,30 @@ object KafkaStreamProcessing {
     props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
 
     val builder = new StreamsBuilder()
-    val stream: KStream[String, String] = builder.stream[String, String]("iot-input-topic")(Consumed.`with`(Serdes.String, Serdes.String))
+    val stream: KStream[String, String] = builder.stream[String, String]("the_stream")(Consumed.`with`(Serdes.String, Serdes.String))
 
     val updatedStream = stream.mapValues { value =>
       IoTDataJson.deserialize(value) match {
         case Right(data) => IoTDataJson.serialize(updateAlerte(data))
         case Left(error) =>
-          println(s"Deserialization error: ${error.getMessage}") // Log error
-          value // Pass through the original message in case of error
+          println(s"Deserialization error: ${error.getMessage}")
+          value
       }
     }
 
-    updatedStream.to("iot-output-topic")(Produced.`with`(Serdes.String, Serdes.String))
+    updatedStream.to("the_second_stream")(Produced.`with`(Serdes.String, Serdes.String))
 
     val streams = new KafkaStreams(builder.build(), props)
     streams.start()
-
     sys.ShutdownHookThread {
       streams.close()
     }
+    streams
   }
 
   def updateAlerte(iotData: IoTData): IoTData = {
-    val co2Alert = iotData.qualiteAir.CO2 >= 300000000 // 300 million ppm
-    val particulesFinesAlert = iotData.qualiteAir.particulesFines >= 25 // 25 µg/m³
-
+    val co2Alert = iotData.qualiteAir.CO2 >= 300000000
+    val particulesFinesAlert = iotData.qualiteAir.particulesFines >= 100
     if (co2Alert || particulesFinesAlert) {
       iotData.copy(alerte = "Yes")
     } else {
